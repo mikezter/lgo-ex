6f17f2e0c043dca47a566a5f48d58eb8a9adfcb7
Michael Strüder <mikezter@gmail.com>
2013-10-30 00:17:52 +0100

    initial

---
+++ q2_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "fmt"
+)
+
+func ExampleArray() {
+
+  a := make([]int, 10)
+  for i := 0; i < 10; i++ {
+    a[i] = i
+  }
+  fmt.Printf("%v ", a)
+  // Output: [0 1 2 3 4 5 6 7 8 9]
+}
+
+func ExampleForloop() {
+  for i := 0; i < 10; i++ {
+    fmt.Printf("%v ", i)
+  }
+  // Output: 0 1 2 3 4 5 6 7 8 9
+}
+
+func ExampleGoto() {
+  i := 0
+Loop:
+  fmt.Printf("%v ", i)
+  i++
+  if i < 10 {
+    goto Loop
+  }
+  // Output: 0 1 2 3 4 5 6 7 8 9
+}
