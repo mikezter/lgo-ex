40fb8fa670e280166b45a2bade6f3802ce5cc0af
Michael Strüder <mikezter@gmail.com>
2013-11-03 23:03:24 +0100

    change stuff

--- q11_test.go
+++ q11_test.go
@@ -5,19 +5,21 @@ import "fmt"
 func fibo(n int) int {
   switch n {
   case 1, 2:
+    fmt.Print("1 ")
     return 1
   default:
-    return fibo(n-1) + fibo(n-2)
+    v := fibo(n-1) + fibo(n-2)
+    fmt.Printf("%v ", v)
+    return v
   }
 }
 
 func ExampleFibo() {
 
   n := 30
+  fibo(n)
   for i := n + 1; i < n+3; i++ {
 
-    fmt.Print(fibo(i))
-    fmt.Print(" ")
   }
   // Output: 1 1 2 3 5 8 13
 }
